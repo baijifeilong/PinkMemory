78dcd6f10933fb00daf14c6345db4a9554c86dcb
BaiJiFeiLong <baijifeilong@gmail.com>
2019-05-10 14:12:59 +0800

    Add adbd controller

--- app/src/main/java/io/github/baijifeilong/pinkmemory/MainActivity.kt
+++ app/src/main/java/io/github/baijifeilong/pinkmemory/MainActivity.kt
@@ -1,65 +1,88 @@
 package io.github.baijifeilong.pinkmemory
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
-import org.jetbrains.anko.recyclerview.v7.recyclerView
-import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 import java.util.*
-import kotlin.collections.ArrayList
 
+@SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity(), AnkoLogger {
-    val list = ArrayList<String>()
-    lateinit var recyclerView: RecyclerView;
+
+    private lateinit var switch: Switch
+
+    private fun fetchIp(): String {
+        val cmd = "ip route get 1.2.3.4"
+        return Scanner(Runtime.getRuntime().exec(cmd).inputStream).useDelimiter("\\A").next().split("\\s+".toRegex())[6]
+    }
+
+    private fun fetchAdbPort(): String? {
+        val cmd = "getprop service.adb.tcp.port"
+        val port = try {
+            Scanner(Runtime.getRuntime().exec(cmd).inputStream).useDelimiter("\\A").next().trim()
+        } catch (e: NoSuchElementException) {
+            null
+        }
+        return if ("-1" == port) null else port
+    }
+
+    private fun startAdb() {
+        val runtime = Runtime.getRuntime().exec("su")
+        runtime.outputStream.write("setprop service.adb.tcp.port 5555\n".toByteArray())
+        runtime.outputStream.write("start adbd\n".toByteArray())
+        runtime.outputStream.write("exit\n".toByteArray())
+        runtime.outputStream.flush()
+        val scanner = Scanner(runtime.inputStream).useDelimiter("\\A");
+        val output = if (scanner.hasNext()) scanner.next() else null
+        toast("ADB已启用, 控制台输出: $output")
+    }
+
+    private fun stopAdb() {
+        val runtime = Runtime.getRuntime().exec("su")
+        runtime.outputStream.write("setprop service.adb.tcp.port -1\n".toByteArray());
+        runtime.outputStream.write("stop adbd\n".toByteArray());
+        runtime.outputStream.write("exit\n".toByteArray())
+        runtime.outputStream.flush()
+        val scanner = Scanner(runtime.inputStream).useDelimiter("\\A");
+        val output = if (scanner.hasNext()) scanner.next() else null
+        toast("ADB已禁用, 控制台输出: $output")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        verticalLayout {
-            recyclerView {
-                recyclerView = this
-                layoutManager = LinearLayoutManager(this@MainActivity)
-                adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-                    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-                        return object : RecyclerView.ViewHolder(
-                            LayoutInflater.from(this@MainActivity).inflate(
-                                android.R.layout.simple_list_item_1,
-                                parent,
-                                false
-                            )
-                        ) {}
-                    }
-
-                    override fun getItemCount(): Int {
-                        return list.size
-                    }
-
-                    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-                        holder.itemView.findViewById<TextView>(android.R.id.text1).text = list[position]
-                    }
+        relativeLayout {
+            switch {
+                switch = this
+                info { "CLICKED" }
+                onClick {
+                    switch.isEnabled = false
+                    if (fetchAdbPort() == null) startAdb() else stopAdb()
+                    switch.isEnabled = true
+                    refreshLayout()
                 }
+            }.lparams {
+                centerInParent()
             }
         }
+    }
 
-        Timer().scheduleAtFixedRate(object : TimerTask() {
-            var count = 0
-            override fun run() {
-                ++count
-                runOnUiThread {
-                    "OK $count".apply {
-                        println(this)
-                        info { this }
-                        list.add(this)
-                        recyclerView.adapter!!.notifyDataSetChanged()
-                    }
-                }
-            }
-        }, 0, 1000)
+    override fun onResume() {
+        super.onResume()
+        refreshLayout()
+    }
+
+    private fun refreshLayout() {
+        val port = fetchAdbPort()
+        val ip = fetchIp()
+        if (port == null) {
+            switch.text = "已禁用"
+            switch.isChecked = false
+        } else {
+            switch.text = "已启用($ip:$port)"
+            switch.isChecked = true
+        }
     }
 }
